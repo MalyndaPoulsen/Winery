11c818f8ec80b9b79995bfa2f55edeb180867fde
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-08-06 15:34:24 -0600

    component merge

--- components/wineClub-component.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    angular.module('Winery')
-        .component('wineclubComponent', {
-            templateUrl: 'components/wineclub-component.html',
-          
-        })
-
-
-}());
